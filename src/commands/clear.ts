2ae33ea0e03fed41ef4431eff7a9a3d98085af6a
Ja1z666 <thatsameuser@icloud.com>
2023-08-12 15:24:46 +0300

    clear command

---
+++ src/commands/clear.ts
@@ -0,0 +1,31 @@
+import { ChannelType, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { SlashCommand } from "../types";
+
+const ClearCommand: SlashCommand = {
+  command: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Delets messages from the current channel")
+    .addIntegerOption(option => {
+      return option
+        .setMaxValue(100)
+        .setMinValue(1)
+        .setName("count")
+        .setDescription("Message amount to be cleared")
+        .setRequired(true);
+    })
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+  execute: interaction => {
+    const count = interaction.options.getInteger("count") as number;
+
+    interaction.channel?.messages.fetch({ limit: count }).then(async msg => {
+      if (interaction.channel?.type === ChannelType.DM) return;
+
+      const deleted_msg = await interaction.channel?.bulkDelete(msg, true);
+
+      interaction.reply({ content: `Successfully deleted ${deleted_msg?.size} message(s)`, ephemeral: true });
+    })
+  },
+  cooldown: 10
+}
+
+export default ClearCommand;
