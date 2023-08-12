3efc83fdd341f96c90d967e9cced096604ab49c8
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2023-08-12 15:11:39 +0300

    Update profile.ts

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -1,32 +1,33 @@
-import { AttachmentBuilder, SlashCommandBuilder } from "discord.js"
+import { AttachmentBuilder, SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "../types";
 import Canvas from "@napi-rs/canvas";
 import { request } from "undici";
+const { join } = require("path");
 
 const command: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName("profile")
-    .addUserOption(option => {
-      return option
-        .setName("user")
-        .setDescription("User")
-        .setRequired(false)
+    .addUserOption((option) => {
+      return option.setName("user").setDescription("User").setRequired(false);
     })
-    .setDescription("Return user profile")
-  ,
+    .setDescription("Return user profile"),
   execute: async (interaction) => {
     const user = interaction.options.getUser("user");
 
     const canvas = Canvas.createCanvas(700, 250);
-    const context = canvas.getContext('2d');
+    const context = canvas.getContext("2d");
+    Canvas.GlobalFonts.registerFromPath(
+      join(__dirname, "../../", "fonts", "Roboto.ttf"),
+      "Roboto"
+    );
+    context.font = "28px Roboto";
+    context.strokeStyle = "#ffffff";
+    context.fillStyle = "#ffffff";
+    context.fillText("Profile", canvas.width / 2.5, canvas.height / 3.5);
 
-    Canvas.GlobalFonts.registerFromPath('../../fonts/Roboto.ttf', 'Roboto');
-    context.font = '28px sans-serif';
-    context.strokeStyle = '#ffffff';
-    context.fillStyle = '#ffffff';
-    context.fillText('Profile', canvas.width / 2.5, canvas.height / 3.5);
-
-    const { body } = await request(interaction.user.displayAvatarURL({ extension: 'jpg' }));
+    const { body } = await request(
+      interaction.user.displayAvatarURL({ extension: "jpg" })
+    );
     const avatar = await Canvas.loadImage(await body.arrayBuffer());
 
     context.beginPath();
@@ -35,11 +36,13 @@ const command: SlashCommand = {
     context.clip();
     context.drawImage(avatar, 25, 25, 200, 200);
 
-    const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile.png' });
+    const attachment = new AttachmentBuilder(canvas.toBuffer("image/png"), {
+      name: "profile.png",
+    });
 
     interaction.reply({ files: [attachment] });
   },
-  cooldown: 10
-}
+  cooldown: 10,
+};
 
-export default command
+export default command;
